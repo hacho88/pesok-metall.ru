20eff4c4133185eb9ea72ab87a4e86c46db5b876
Kairat <sariev.kairat@gmail.com>
2026-09-11 15:33:24 +0300

    chore: убрать временный скрипт

--- scripts/check-notify-settings.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const p = new PrismaClient();
-const s = await p.shopSettings.findFirst();
-console.log("maxBotToken:", s?.maxBotToken ? `есть (${s.maxBotToken.slice(0, 8)}...)` : "НЕТ");
-console.log("maxChatId:", s?.maxChatId ?? "НЕТ");
-console.log("messengerType:", s?.messengerType);
-console.log("notifyEmail:", s?.notifyEmail ?? "нет");
-await p.$disconnect();
